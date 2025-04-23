d96b464709a85c24f436aa7639524e0b76687b5b
Gabriel Moura <140829776+GDmour@users.noreply.github.com>
2025-04-23 07:47:51 -0300

    aa

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ document.addEventListener('DOMContentLoaded', function(){
          document.body.style.fontSize = `${tamanhoAtualFonte}rem`
  
      })
+     alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
  
      
  
